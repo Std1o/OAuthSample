7f524baf18bd388de37b7130bc4b191227e4a1bb
Ilya <37378410+Std1o@users.noreply.github.com>
2022-10-31 13:26:51 +0300

    logout

--- app/src/main/java/com/stdio/oauthsample/presentation/ui/activity/MainActivity.kt
+++ app/src/main/java/com/stdio/oauthsample/presentation/ui/activity/MainActivity.kt
@@ -1,13 +1,59 @@
 package com.stdio.oauthsample.presentation.ui.activity
 
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.webkit.CookieManager
+import android.webkit.CookieSyncManager
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.NavOptions
+import androidx.navigation.findNavController
 import com.stdio.oauthsample.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        super.onOptionsItemSelected(item)
+        when (item.itemId) {
+            R.id.action_logout -> {
+                clearCookies(this)
+                findNavController(R.id.fragmentContainerView2).navigate(
+                    R.id.bankListFragment,
+                    Bundle(),
+                    NavOptions.Builder()
+                        .setPopUpTo(R.id.bankListFragment, true)
+                        .build()
+                )
+            }
+        }
+        return true
+    }
+
+    @SuppressWarnings("deprecation")
+    fun clearCookies(context: Context?) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
+            CookieManager.getInstance().removeAllCookies(null)
+            CookieManager.getInstance().flush()
+        } else if (context != null) {
+            val cookieSyncManager = CookieSyncManager.createInstance(context)
+            cookieSyncManager.startSync()
+            val cookieManager: CookieManager = CookieManager.getInstance()
+            cookieManager.removeAllCookie()
+            cookieManager.removeSessionCookie()
+            cookieSyncManager.stopSync()
+            cookieSyncManager.sync()
+        }
     }
 }
